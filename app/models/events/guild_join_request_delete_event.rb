5b92c55ebaf3f7c791d0765f637b6d6b957ecde7
dracco1993 <dracco1993@gmail.com>
2021-11-08 23:44:59 -0500

    Auto annotate models + frozen_string_literal comment

--- app/models/events/guild_join_request_delete_event.rb
+++ app/models/events/guild_join_request_delete_event.rb
@@ -1,2 +1,14 @@
+# frozen_string_literal: true
+
+# == Schema Information
+#
+# Table name: events
+#
+#  id         :bigint           not null, primary key
+#  data       :jsonb
+#  type       :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Events::GuildJoinRequestDeleteEvent < Event
 end
