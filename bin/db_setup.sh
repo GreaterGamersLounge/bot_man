be2a8e0381d114f2efeb95b28e586fa55479be3b
dracco1993 <dracco1993@gmail.com>
2026-02-18 23:29:25 -0500

    chore: Phase 5 - Remove Rails scripts
    Removed bin/ including:
    - rails, rake, setup, spring
    - db_setup.sh

--- bin/db_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-
-# Bot Man DB setup
-docker-compose exec web-bot rake db:drop
-docker-compose exec web-bot rake db:create
-# Do schema setup things here
-docker-compose exec web-bot rake db:migrate
-docker-compose exec web-bot rake db:create RAILS_ENV=test
