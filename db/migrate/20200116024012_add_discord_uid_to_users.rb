e7f05fa6e9d045fb53cbd5c64734bf9fd8c332f8
dracco1993 <dracco1993@gmail.com>
2026-02-18 23:30:27 -0500

    chore: Phase 7 - Remove Rails database directory
    Removed db/ including:
    - schema.rb (duplicated in prisma/schema.prisma)
    - seeds.rb (empty)
    - migrate/*.rb (12 migration files)

--- db/migrate/20200116024012_add_discord_uid_to_users.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddDiscordUidToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :discord_uid, :string
-  end
-end
--- db/migrate/20200425203448_remove_unique_constraint_on_invite_discord_users.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class RemoveUniqueConstraintOnInviteDiscordUsers < ActiveRecord::Migration[6.0]
-  def change
-    remove_index :invite_discord_users, column: %i(invite_id discord_user_uid)
-  end
-end
